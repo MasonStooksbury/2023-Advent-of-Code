e85bc6189dd643ad7e8e2a6a43035dcee636f3fa
Mason Stooksbury <masonstooksbury@gmail.com>
2023-12-09 20:25:43 -0600

    Pushing

--- Code/UNSOLVED-08-Part2.py
+++ Code/UNSOLVED-08-Part2.py
@@ -4,16 +4,16 @@ import shared
 map_input = shared.getInput('08.txt')
 
 
-map_input = '''LR
-
-11A = (11B, XXX)
-11B = (XXX, 11Z)
-11Z = (11B, XXX)
-22A = (22B, XXX)
-22B = (22C, 22C)
-22C = (22Z, 22Z)
-22Z = (22B, 22B)
-XXX = (XXX, XXX)'''.split('\n')
+##map_input = '''LR
+##
+##11A = (11B, XXX)
+##11B = (XXX, 11Z)
+##11Z = (11B, XXX)
+##22A = (22B, XXX)
+##22B = (22C, 22C)
+##22C = (22Z, 22Z)
+##22Z = (22B, 22B)
+##XXX = (XXX, XXX)'''.split('\n')
 
 
 
@@ -41,6 +41,7 @@ for key in node_dict.keys():
     if key[-1] == 'A':
         starting_positions.append(key)
 
+
 def positionsEndInZ(positions):
     for position in positions:
         if position[-1] != 'Z':
@@ -53,20 +54,47 @@ new_positions = starting_positions
 outer_break = False
 instruction = 0
 
-while not positionsEndInZ(new_positions):
-    other = []
-    for position in new_positions:
-        other.append(node_dict[position][instructions[instruction]])
-    instruction += 1
-    new_positions = other
-    count += 1
-    if positionsEndInZ(other):
-        outer_break = True
-        break
-    if instruction > len(instructions) -1:
-        instruction = 0
+##while not positionsEndInZ(new_positions):
+##    other = []
+##    for position in new_positions:
+##        other.append(node_dict[position][instructions[instruction]])
+##    instruction += 1
+##    new_positions = other
+##    count += 1
+##    if positionsEndInZ(other):
+##        outer_break = True
+##        break
+##    if instruction > len(instructions) -1:
+##        instruction = 0
+##
+##print(count)
 
-print(count)
+
+def getShit(position):
+    start = position
+    next_thing = start
+    count = 0
+    instruction = 0
+    while next_thing[-1] != 'Z':
+        for instruction in instructions:
+            next_thing = node_dict[next_thing][instruction]
+            if next_thing[-1] == 'Z':
+                count += 1
+                break
+        count += 1
+    return count
+
+print(instructions)
+
+values = []
+total = 1
+for position in starting_positions:
+    values.append(getShit(position))
+values.sort()
+values.pop(0)
+for value in values:
+    total *= value
+print(total)
 
 
 # while not positionsEndInZ(new_positions):
--- Code/test.py
+++
@@ -1,8 +0,0 @@
-#! python3
-
-
-instruction = 0
-
-for thing in range(10):
-    instruction %= instruction + 2
-    print(instruction)
